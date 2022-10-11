7de215582ceb5dd606839112a2379bb555e48582
PizzaDeliveryOP <114107987+PizzaDeliveryOP@users.noreply.github.com>
2022-10-10 20:39:15 -0400

    Add files via upload

--- battle_logic.py
+++ battle_logic.py
@@ -1,75 +1,72 @@
+#Imports all the necessary items for this code. The code should import everything with import *. Sometimes it 
+#does not. To appease the dark spirits that run python, do not remove the 'import name' section. It should 
+#not be needed, but when you remove it the program sometimes crashes. 
 from unicodedata import name
 from characters import *
 import random
-
 #We do not talk about fight loop. We do describe it in the comments. This code takes the enemy dictionary 
 #and prepares it to be "fought" against by the hero dictionary. 
 def fight_loop(monster_list):
     while len(monster_list) > 0:
         current_monster = monster_list[0]
-        print('A wild', current_monster['name'],'appears. Prepare to fight. You have', (your_hero["health"]),'health remaining')
+        print('You are facing a', current_monster['name'],'. Prepare to fight. You have', (your_hero["health"]),'health remaining')
+        print('Your foe has', current_monster['health'],'health remaining.')
         # select a random attack ffrom the hero
-        # decrement the monster's health based on the randmoly selected hero attack
+        # decrement the monster's health based on the randmoly selected hero attack.
+        # Hero is given the first strike. This made the programming easier and could say something about the 
+        # open hostility of action heroes, but ease of code is the real answer.
         random_attack = random.choice(your_hero['attacks'])
+        #This helps track the damage and attacks being used on the monster visually for the user story.
+        print('You strike at the',current_monster['name'],'with a',random_attack[0],'dealing',random_attack[1],'damage.')
         current_monster['health'] -= random_attack[1]
-        print(random_attack)
         if current_monster['health']<= 0:
             print('Your foe the', current_monster['name'], 'lies dead. Steel yourself for the next one.')
+            your_hero['coins']['copper']+=current_monster['coins']['copper']
+            your_hero['coins']['silver']+=current_monster['coins']['silver']
+            your_hero['coins']['gold']+=current_monster['coins']['gold']
+            print('You now have the following funds from looting the monster:',your_hero['coins'])
+            print(your_hero['coins'])
+            #Do NOT Change the below code. It adds the monster's equipment to the hero's equipment, and took a solid day.
+            #Syntax is very important in python. It does not print excellently, but it works! 
+            monster_inv = current_monster['equipment']
+            your_hero["equipment"].update(monster_inv)
+            print('Your loot the monster, and now have:', your_hero['equipment'])
+            #levels up the hero if they defeat the monster. This is part of the bonus task of levelling up the hero. 
+            your_hero['level']+=1
+            print('Level up! You are now level',your_hero['level'])
             monster_list.pop(0)
             continue
+        #continues the fight if monster health is above zero. 
         elif current_monster['health']>0:
             random_attack = random.choice(current_monster['attacks'])
+            print('The monstrous',current_monster['name'],'strikes at you with a',random_attack[0],'dealing',random_attack[1],'damage.')
             your_hero['health'] -= random_attack[1]
-            print(random_attack)
+        #ends the program early if the hero dies
         if your_hero['health']<=0:
             print("You Died")
-            break
+            return
         elif len(monster_list) == 0:
             print("Your foes lie defeated. You win or something.")
-
-    #for i in monster_book_of_monsters:
-        #for i,d in enumerate(monster_book_of_monsters):
-            #print(i," : ",d)
-        
-    #if your_hero["health"] <= 0:
-        #print('You Died')
-#for health_tuple in enemy_0001['health']:
-
-    
-
         # # HERO gets attack first
         # did_attack_num = random.randint(0, 10)
         # if (did_attack_num > 3):
         #     # hero attack hit!
-        
         # did_attack_num = random.randint(0, 10)
         # if(did_attack_num >= 5):
         #     # villian
-
-#Manages the Hero starting health and their Attack loop
-#print(your_hero["health"])
-#print(random.choice(your_hero["attacks"]))
-
-#Manages the Hero starting health and their Attack loop
-#print(your_hero["health"])
-#print(random.choice(your_hero["attacks"]))
-
-
-fight_loop(monster_book_of_monsters)
-
-
-#Randomly select a monster
-#print(random.choice(monster_book_of_monsters["name"]))
-
-#Manages the break of game if the players health reaches zero or becomes negative. 
-
-
-#def run_game():
-#print('Good luck adventurer! You are without gold and alone, luck is all you have...')
-
-    #run_game()
-    #Welcome Message
-    #Fight Enemy 1
-    #Fight Enemy 2
-    #Fight Enemy 3
-    #Print Victory Message
+def greeting_message():
+    print('Good luck adventurer! You are without gold and alone, luck is all you have...')
+def victory_message():
+    if your_hero['health']<=0:
+        print('Your journey ends here. Your bones are swept clean by the desert sands. Press continue to continue')
+    else:print("Your foes lie defeated. You win or something.")
+#The small cycle of functions contained in run_game...run the game. 
+def run_game():
+#Starts the game with a message to the user
+    greeting_message()
+#begins the fighting/looting loop
+    fight_loop(monster_book_of_monsters)
+#displays a victory message if the hero successfully kills all monsters while above 0 health
+    victory_message()
+#Calls the above function of functions to run the actual game. See this as a 'press Start' for the program.
+run_game()
--- characters.py
+++ characters.py
@@ -1,22 +1,3 @@
-#(5 points): As a data analyst, I want to make at least five commits on GitHub with descriptive messages.  
-#(2.5 points): As a user, I want an engaging story to be told using print() statements.  
-    #(2.5 points): As a data analyst, I want to create a hero variable and set it equal to a Dictionary  
-    #(2.5 points): As a data analyst, I want to create THREE enemy variables and set each equal to a Dictionary 
-#(5 points): As a user, I want my Hero and Enemy’s attack to be chosen at random. 
-#(5 points): As a user, I want my Hero or Enemy’s health to decrease based on the power of the successful attack. 
-#(2.5 points): As a user, I want the results of each attack to be printed to the terminal.
-#(5 points): As a user, I want the Hero and Enemy to continue attacking each other until one's health reaches zero.  
-#(5 points): As a user, I want to be able to “loot” defeated enemies by adding the Enemy’s equipment set to the Hero’s equipment Set
-#(5 points): As a data analyst, I want the game to conclude when the Hero’s health reaches 0, or when all three Enemies have been defeated.   
-#(2.5 points): As a data analyst, I want to create a run_game() function, which calls my other functions in a logical order to determine the game’s flow. 
-#(5 points): As a data analyst, I want all of my functions to have a Single Responsibility. 
-
-#Bonus Stories
-    #(5 points): As a user, I want to “loot” defeated enemies by adding the Enemy’s gold, silver, and copper coins to the appropriate value in the Hero’s coin Dictionary.
-    #(7.5 points): As a user, I want to increase my Hero’s level after defeating an Enemy, which will include: 
-        #Incrementing my Hero’s level by 1 
-        #Adding a new attack of my choosing to my Hero's Attack Tuple. 
-
 your_hero = {
     "name": 'Depraved', #String
     "level": 1, #integer
@@ -29,20 +10,20 @@ your_hero = {
         "gold": 0, #integer
     }
 }
-
+#Enemy 1 dictionary. 
 enemy_0001 = {
 "name": 'Monster House',
     "level": 1, #integer
     "health": 100, #integer
     "equipment": {'Chain Mail', 'Helmet','Ornate Brass Doorknobs'}, #set of strings
-    "attacks": (('Foul Horror Shutters',15),('Fire Misplace',35),('Witch Slaying Leap',45)),
+    "attacks": (('Foul Horror Shutters',15),('Fire Misplaced',35),('Witch Slaying Leap',45)),
     "coins": { #Dictionary
-        "copper": 0, #integer
+        "copper": 10, #integer
         "silver": 10, #integer
         "gold": 0, #integer    
     }
 }
-
+#Enemy 2 dictionary
 enemy_0002 = {
 "name": 'Shimmer',
     "level": 1, #integer
@@ -51,23 +32,23 @@ enemy_0002 = {
     "attacks": (('Daylight Sneak Attack',15),('Jump Scare',0)),
     "coins": { #Dictionary
         "copper": 0, #integer
-        "silver": 10, #integer
+        "silver": 20, #integer
         "gold": 0, #integer    
     }
 }
-
+#Enemy 3 dictionary
 enemy_0003 = {
 "name": 'Land Shark',
     "level": 1, #integer
     "health": 100, #integer
     "equipment": {'Large Tooth', 'Small Tooth','Golden Lock Tooth'}, #set of strings
-    "attacks": (('Bite',15),('Insult Family',35)),
+    "attacks": (('Bite',15),('Devastating Insult',35)),
     "coins": { #Dictionary
         "copper": 0, #integer
         "silver": 10, #integer
-        "gold": 0, #integer    
+        "gold": 10, #integer    
     }
 }
-
+#dictionary of dictionaries. Allows for quick address of monster dictionaries in sequence.
 monster_book_of_monsters = [enemy_0003,enemy_0002,enemy_0001]
 
